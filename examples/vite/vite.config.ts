ed8a9d8e04b35b30bc63197c86d33aef0cf2aee9
markthree <1801982702@qq.com>
2023-05-21 13:07:16 +0800

    refactor: more user-friendly development experience

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -4,5 +4,6 @@ import Inspect from 'vite-plugin-inspect'
 import Modules from 'vite-plugin-use-modules'
 
 export default defineConfig({
+	clearScreen: false,
 	plugins: [Vue(), Inspect(), Modules({ auto: true })]
 })
